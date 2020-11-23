edc86364a0839fec02cd52f0aa2f2281a6ff50ed
Miloš Popović <gpopac@gmail.com>
2020-11-23 04:09:27 +0100

    Fix camera bug, if screen is rotated after starting Camera.

--- app/src/main/java/org/biologer/biologer/adapters/CameraActivity.java
+++ app/src/main/java/org/biologer/biologer/adapters/CameraActivity.java
@@ -14,9 +14,12 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
 
 import androidx.core.content.FileProvider;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import com.bumptech.glide.Glide;
 
 import java.io.File;
 import java.io.IOException;
@@ -27,6 +30,7 @@ import java.util.Locale;
 public class CameraActivity extends Activity {
 
     private static final String TAG = "Biologer.Camera";
+    boolean screen_rotated = false;
     int CAMERA = 1;
     Uri uri;
 
@@ -36,19 +40,10 @@ public class CameraActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Log.d(TAG, "Starting CameraActivity");
-
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            Log.i(TAG, "There is Camera software installed. All ready to take picture!");
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                takePictureIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            }
-            uri = getPhotoUri(getImageFileName());
-            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
-            startActivityForResult(takePictureIntent, CAMERA);
-        } else {
-            Log.e(TAG, "Take picture intent could not start for some reason.");
+        // Restore images on screen rotation...
+        if (savedInstanceState != null) {
+            screen_rotated = savedInstanceState.getBoolean("screen_rotated");
+            uri = Uri.parse(savedInstanceState.getString("uri"));
         }
 
         receiver = new BroadcastReceiver() {
@@ -71,6 +66,33 @@ public class CameraActivity extends Activity {
                 }
             }
         };
+
+        if (!screen_rotated) {
+            Log.d(TAG, "Starting CameraActivity");
+
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                Log.i(TAG, "There is Camera software installed. All ready to take picture!");
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    takePictureIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                }
+                uri = getPhotoUri(getImageFileName());
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                startActivityForResult(takePictureIntent, CAMERA);
+            } else {
+                Log.e(TAG, "Take picture intent could not start for some reason.");
+            }
+        }
+    }
+
+    // When screen is rotated activity is destroyed, thus images should be saved and opened again!
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver);
+        outState.putBoolean("screen_rotated", true);
+        outState.putString("uri", uri.toString());
+        Log.d(TAG, "Saving the state of Camera Activity.");
+        super.onSaveInstanceState(outState);
     }
 
     @Override
