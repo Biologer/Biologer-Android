2696be76e796e57c175d631ec8fe98a60827fa99
gpopac <gpopac@gmail.com>
2023-05-25 01:09:43 +0200

    Add transparent view behind floating button to prevent accidental clicks.

---
+++ app/src/main/java/org/biologer/biologer/adapters/EntryRecycleView.java
@@ -0,0 +1,22 @@
+package org.biologer.biologer.adapters;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class EntryRecycleView extends RecyclerView {
+    public EntryRecycleView(@NonNull Context context) {
+        super(context);
+    }
+
+    public EntryRecycleView(@NonNull Context context, @Nullable @org.jetbrains.annotations.Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public EntryRecycleView(@NonNull Context context, @Nullable @org.jetbrains.annotations.Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
