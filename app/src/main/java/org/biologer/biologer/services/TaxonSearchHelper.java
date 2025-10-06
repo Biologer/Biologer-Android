6745ae05d71a658d47b436a33c3c410a3d18a3ea
gpopac <gpopac@gmail.com>
2025-10-06 20:28:41 +0200

    Improve Taxon search logic a bit...

--- app/src/main/java/org/biologer/biologer/services/TaxonSearchHelper.java
+++ app/src/main/java/org/biologer/biologer/services/TaxonSearchHelper.java
@@ -18,8 +18,12 @@ import io.objectbox.Box;
 import io.objectbox.BoxStore;
 import io.objectbox.query.Query;
 import io.objectbox.query.QueryBuilder;
+import io.objectbox.query.QueryCondition;
+
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class TaxonSearchHelper {
 
@@ -35,6 +39,7 @@ public class TaxonSearchHelper {
         }
 
         List<TaxonDb> allTaxaLists = new ArrayList<>();
+        Set<Long> allTaxaIds = new HashSet<>(); // For more efficient duplicate search
         String changedEnteredName = searchText.trim();
 
         // Part 1: Query latin names
@@ -46,6 +51,7 @@ public class TaxonSearchHelper {
             String name = getLocalisedLatinName(taxon);
             taxon.setLatinName(name);
             allTaxaLists.add(taxon);
+            allTaxaIds.add(taxon.getId());
         }
 
         // Part 2: Query native names
@@ -57,16 +63,11 @@ public class TaxonSearchHelper {
         for (int i = 0; i < nativeNames.size(); i++) {
             TaxaTranslationDb taxonTranslation = nativeNames.get(i);
             // Don’t add taxa if already on the list
-            boolean duplicated = false;
-            for (int j = 0; j < allTaxaLists.size(); j++) {
-                if (allTaxaLists.get(j).getId() == taxonTranslation.getTaxonId()) {
-                    duplicated = true;
-                }
-            }
-            if (!duplicated) {
+            if (!allTaxaIds.contains(taxonTranslation.getId())) {
                 TaxonDb taxon = getTaxonById(taxonTranslation.getTaxonId());
                 taxon.setLatinName(getLocalisedLatinName(taxon));
                 allTaxaLists.add(taxon);
+                allTaxaIds.add(taxon.getId());
             }
         }
 
@@ -77,16 +78,11 @@ public class TaxonSearchHelper {
         for (int i = 0; i < synonymsList.size(); i++) {
             SynonymsDb synonym = synonymsList.get(i);
             // Don’t add taxa if already on the list
-            boolean duplicated = false;
-            for (int j = 0; j < allTaxaLists.size(); j++) {
-                if (allTaxaLists.get(j).getId() == synonym.getTaxonId()) {
-                    duplicated = true;
-                }
-            }
-            if (!duplicated) {
+            if (!allTaxaIds.contains(synonym.getTaxonId())) {
                 TaxonDb taxon = getTaxonById(synonym.getTaxonId());
                 taxon.setLatinName(getLocalisedLatinName(synonym));
                 allTaxaLists.add(taxon);
+                allTaxaIds.add(taxon.getId());
             }
         }
 
@@ -100,6 +96,7 @@ public class TaxonSearchHelper {
         }
 
         List<TaxonDb> allTaxaLists = new ArrayList<>();
+        Set<Long> allTaxaIds = new HashSet<>(); // For more efficient duplicate search
         String changedEnteredName = searchText.trim();
 
         // Part 1: Query latin names
@@ -111,29 +108,24 @@ public class TaxonSearchHelper {
             String name = getLocalisedLatinName(taxon);
             taxon.setLatinName(name);
             allTaxaLists.add(taxon);
+            allTaxaIds.add(taxon.getId());
         }
 
         // Part 2: Query native names
         //
         List<TaxaTranslationDb> nativeNames = getTaxaFromNativeName(changedEnteredName);
-
         // Check all taxon translations and add only those with IDs that were not
         // already queried by latin name
         for (int i = 0; i < nativeNames.size(); i++) {
             TaxaTranslationDb taxonTranslation = nativeNames.get(i);
             // Don’t add taxa if already on the list
-            boolean duplicated = false;
-            for (int j = 0; j < allTaxaLists.size(); j++) {
-                if (allTaxaLists.get(j).getId() == taxonTranslation.getTaxonId()) {
-                    duplicated = true;
-                }
-            }
-            if (!duplicated) {
-                TaxonDb taxon = getTaxonById(taxonTranslation.getTaxonId());
+            if (!allTaxaIds.contains(taxonTranslation.getId())) {
                 // Check if the taxon belongs to the group and add it
+                TaxonDb taxon = getTaxonById(taxonTranslation.getTaxonId());
                 if (taxon.getGroups().contains(String.valueOf(group))) {
                     taxon.setLatinName(getLocalisedLatinName(taxon));
                     allTaxaLists.add(taxon);
+                    allTaxaIds.add(taxon.getId());
                 }
             }
         }
@@ -141,26 +133,22 @@ public class TaxonSearchHelper {
         // Part 3: Query synonyms
         //
         List<SynonymsDb> synonymsList = getTaxaFromSynonymName(changedEnteredName);
-
         for (int i = 0; i < synonymsList.size(); i++) {
             SynonymsDb synonym = synonymsList.get(i);
             // Don’t add taxa if already on the list
-            boolean duplicated = false;
-            for (int j = 0; j < allTaxaLists.size(); j++) {
-                if (allTaxaLists.get(j).getId() == synonym.getTaxonId()) {
-                    duplicated = true;
-                }
-            }
-            if (!duplicated) {
+            if (!allTaxaIds.contains(synonym.getTaxonId())) {
                 TaxonDb taxon = getTaxonById(synonym.getTaxonId());
                 // Check if the taxon belongs to the group and add it
                 if (taxon.getGroups().contains(String.valueOf(group))) {
                     taxon.setLatinName(getLocalisedLatinName(synonym));
                     allTaxaLists.add(taxon);
+                    allTaxaIds.add(taxon.getId());
                 }
             }
         }
+
         return allTaxaLists;
+
     }
 
     private List<SynonymsDb> getTaxaFromSynonymName(String name) {
@@ -205,63 +193,49 @@ public class TaxonSearchHelper {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
         String localeScript = Localisation.getLocaleScript();
 
-        Box<TaxaTranslationDb> taxaTranslationDataBox = App.get().getBoxStore().boxFor(TaxaTranslationDb.class);
-        List<TaxaTranslationDb> nativeList;
+        Box<TaxaTranslationDb> box = App.get().getBoxStore().boxFor(TaxaTranslationDb.class);
 
-        // For Serbian language we should also search for Latin and Cyrillic names
-        if (localeScript.equals("sr")) {
-            if (preferences.getBoolean("english_names", false)) {
-                Query<TaxaTranslationDb> nativeQuery = taxaTranslationDataBox
-                        .query(TaxaTranslationDb_.locale.equal("en")
-                                .and(TaxaTranslationDb_.nativeName.contains(changedEnteredName,
-                                        QueryBuilder.StringOrder.CASE_INSENSITIVE))
-                                .or(TaxaTranslationDb_.locale.equal("sr")
-                                        .and(TaxaTranslationDb_.nativeName.contains(changedEnteredName,
-                                                QueryBuilder.StringOrder.CASE_INSENSITIVE)))
-                                .or(TaxaTranslationDb_.locale.equal("sr-Latn")
-                                        .and(TaxaTranslationDb_.nativeName.contains(changedEnteredName,
-                                                QueryBuilder.StringOrder.CASE_INSENSITIVE))))
-                        .build();
-                nativeList = nativeQuery.find(0, 10);
-                nativeQuery.close();
+        boolean includeEnglish = preferences.getBoolean("english_names", false);
+
+        QueryCondition<TaxaTranslationDb> cond;
+
+        if ("sr".equals(localeScript)) {
+            // Grouped: (sr AND name) OR (sr-Latn AND name) [OR (en AND name)]
+            QueryCondition<TaxaTranslationDb> cSr = TaxaTranslationDb_.locale.equal("sr")
+                    .and(TaxaTranslationDb_.nativeName.contains(changedEnteredName,
+                            QueryBuilder.StringOrder.CASE_INSENSITIVE));
+            QueryCondition<TaxaTranslationDb> cSrLatn = TaxaTranslationDb_.locale.equal("sr-Latn")
+                    .and(TaxaTranslationDb_.nativeName.contains(changedEnteredName,
+                            QueryBuilder.StringOrder.CASE_INSENSITIVE));
+
+            if (includeEnglish) {
+                QueryCondition<TaxaTranslationDb> cEn = TaxaTranslationDb_.locale.equal("en")
+                        .and(TaxaTranslationDb_.nativeName.contains(changedEnteredName,
+                                QueryBuilder.StringOrder.CASE_INSENSITIVE));
+                cond = cEn.or(cSr).or(cSrLatn);
             } else {
-                Query<TaxaTranslationDb> nativeQuery = taxaTranslationDataBox
-                        .query(TaxaTranslationDb_.locale.equal("sr")
-                                .and(TaxaTranslationDb_.nativeName.contains(changedEnteredName,
-                                        QueryBuilder.StringOrder.CASE_INSENSITIVE))
-                                .or(TaxaTranslationDb_.locale.equal("sr-Latn")
-                                        .and(TaxaTranslationDb_.nativeName.contains(changedEnteredName,
-                                                QueryBuilder.StringOrder.CASE_INSENSITIVE))))
-                        .build();
-                nativeList = nativeQuery.find(0, 10);
-                nativeQuery.close();
+                cond = cSr.or(cSrLatn);
+            }
+
+        } else {
+            // Grouped: (local AND name) [OR (en AND name)]
+            QueryCondition<TaxaTranslationDb> cLocal = TaxaTranslationDb_.locale.equal(localeScript)
+                    .and(TaxaTranslationDb_.nativeName.contains(changedEnteredName,
+                            QueryBuilder.StringOrder.CASE_INSENSITIVE));
+
+            if (includeEnglish) {
+                QueryCondition<TaxaTranslationDb> cEn = TaxaTranslationDb_.locale.equal("en")
+                        .and(TaxaTranslationDb_.nativeName.contains(changedEnteredName,
+                                QueryBuilder.StringOrder.CASE_INSENSITIVE));
+                cond = cEn.or(cLocal);
+            } else {
+                cond = cLocal;
             }
         }
 
-        // For other languages it is more simple...
-        else {
-            if (preferences.getBoolean("english_names", false)) {
-                Query<TaxaTranslationDb> nativeQuery = taxaTranslationDataBox
-                        .query(TaxaTranslationDb_.locale.equal("en")
-                                .and(TaxaTranslationDb_.nativeName.contains(changedEnteredName,
-                                        QueryBuilder.StringOrder.CASE_INSENSITIVE))
-                                .or(TaxaTranslationDb_.locale.equal(localeScript)
-                                        .and(TaxaTranslationDb_.nativeName.contains(changedEnteredName,
-                                                QueryBuilder.StringOrder.CASE_INSENSITIVE))))
-                        .build();
-                nativeList = nativeQuery.find(0, 10);
-                nativeQuery.close();
-            } else {
-                Query<TaxaTranslationDb> nativeQuery = taxaTranslationDataBox
-                        .query(TaxaTranslationDb_.locale.equal(localeScript)
-                                .and(TaxaTranslationDb_.nativeName.contains(changedEnteredName,
-                                        QueryBuilder.StringOrder.CASE_INSENSITIVE)))
-                        .build();
-                nativeList = nativeQuery.find(0, 10);
-                nativeQuery.close();
-            }
+        try (Query<TaxaTranslationDb> query = box.query(cond).build()) {
+            return query.find(0, 10);
         }
-        return nativeList;
     }
 
     private static TaxonDb getTaxonById(long taxon_id) {
